2a31b0f174c65e06010651a2231aa232ff7f76d0
Sean Huber <shuber@einsteinindustries.com>
2013-04-15 17:34:32 -0700

    Initial import

---
+++ kalimba.js
@@ -0,0 +1,46 @@
+var Kalimba = function(notes, container) {
+  this.element = document.createElement('div');
+  this.element.className = 'kalimba';
+
+  if (Kalimba.Tunings[notes]) {
+    this.element.setAttribute('data-tuning', notes);
+    notes = Kalimba.Tunings[notes];
+  }
+
+  this.notes = notes;
+  this.tines = tines(notes)
+
+  this.element.appendChild(this.tines);
+  this.element.appendChild(hole());
+
+  if (container) {
+    this.container = container;
+    this.container.appendChild(this.element);
+  }
+
+  function tines(notes) {
+    var tines = document.createElement('div');
+    tines.className = 'tines';
+    for (i=0, l=notes.length; i < l; i++) {
+      tines.appendChild(tine(notes[i]));
+    }
+    return tines;
+  }
+
+  function tine(note) {
+    var tine = document.createElement('div');
+    tine.className = 'tine';
+    tine.setAttribute('data-note', note);
+    return tine;
+  }
+
+  function hole() {
+    var hole = document.createElement('div');
+    hole.className = 'hole';
+    return hole;
+  }
+}
+
+Kalimba.Tunings = {
+  'alto': ['F#', 'D', 'B', 'G', 'E', 'C', 'A', 'G', 'B', 'D', 'F#', 'A', 'C', 'E', 'G']
+}
